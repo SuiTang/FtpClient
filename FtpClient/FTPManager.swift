f0b53f81294b429e641ea75e75101e8a27d86c95
周泽勇 <zzycami@gmail.com>
2015-04-07 05:32:23 -0700

    init

---
+++ FtpClient/FTPManager.swift
@@ -0,0 +1,17 @@
+//
+//  FTPManager.swift
+//  FtpClient
+//
+//  Created by ZhouZeyong on 15/4/7.
+//  Copyright (c) 2015年 ZhouZeyong. All rights reserved.
+//
+
+import UIKit
+import CFNetwork
+
+@objc protocol FTPManagerDelegate : NSObjectProtocol {
+}
+
+class FTPManager: NSObject {
+   
+}
---
+++ FtpClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FtpClient
+//
+//  Created by ZhouZeyong on 15/4/7.
+//  Copyright (c) 2015年 ZhouZeyong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
